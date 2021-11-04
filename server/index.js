433a8af116b020d53f01d6cc97195c4631bad617
Angus Kellett <anguskellett@hotmail.com>
2021-11-04 22:09:45 +0000

    creates getPlayers function to retrieve data from external api

--- server/index.js
+++ server/index.js
@@ -1,48 +1,65 @@
-const express = require("express")
+const express = require('express')
 const PORT = process.env.PORT || 3001
 const app = express()
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!'})
+// import fetch from "node-fetch"
+
+
+
+const getPlayers = () => {
+  const apiKey = "cb2f338924cc4becafb984ba0862d3e4"
+
+  fetch('https://api.sportsdata.io/v3/mlb/scores/json/Players/TOR', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': `${apiKey}`,
+      'Access-Control-Allow-Origin': '*'
+    }
+  }).then(res => res.json())
+  .then(data => {
+    console.log(data)
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+
+
+
+app.get('/api/players', (req, res) => {
+  getPlayers()
+  //res.json('Hello from the other side')
 })
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
 
-// import fetch from "node-fetch"
+
 // import { config } from 'dotenv';
 // config();
 
 // const apiKey = process.env.API_KEY
 
-// const apiKey = "cb2f338924cc4becafb984ba0862d3e4"
+// let data = ''
+//   const apiKey = "cb2f338924cc4becafb984ba0862d3e4"
 
-// fetch('https://api.sportsdata.io/v3/mlb/scores/json/Players/TOR', {
-//   method: 'GET',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Ocp-Apim-Subscription-Key': `${apiKey}`,
-//     'Access-Control-Allow-Origin': '*'
-//   }
-// }).then(res => res.json())
-// .then(players => {
-//   let picture;
-//   for (let player of players) {
-//     if (player['FirstName'] = 'Vladimir') {
-//       picture = player['PhotoUrl']
+//   fetch('https://api.sportsdata.io/v3/mlb/scores/json/Players/TOR', {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Ocp-Apim-Subscription-Key': `${apiKey}`,
+//       'Access-Control-Allow-Origin': '*'
 //     }
-//   }
-//   document.getElementById('image').innerHTML = picture
-// }).catch((err) => {
-//   console.log(err)
-// })
-
-// const playerName = "Vladimir Guerrero"
-// const playerNumber = "27"
-
-// const addInfo = () => {
-//   document.getElementById('image').innerHTML = `<h1>${playerName}<h1><h3>${playerNumber}<h3>`
-// }
-
-// addInfo()
+//   }).then(res => res.json())
+//   .then(players => {
+//     let picture;
+//     for (let player of players) {
+//       if (player['FirstName'] = 'Vladimir') {
+//         picture = player['PhotoUrl']
+//       }
+//     }
+//     data = picture
+//   }).catch((err) => {
+//     console.log(err)
+//   })
