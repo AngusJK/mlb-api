9c9af3a9a4145ac911e47c87f3841f0ae5ca4207
Angus Kellett <anguskellett@hotmail.com>
2021-11-04 22:10:56 +0000

    removes boilerplate; renders content from call to api/players

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [data, setData] = React.useState(null)
   React.useEffect(() => {
-    fetch('/api')
+    fetch('/api/players')
       .then((res) => res.json())
-      .then((data) => setData(data.message))
+      .then((data) => setData(data))
   }, [])
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <p>{!data ? 'Loading...' : data}</p>
       </header>
     </div>
