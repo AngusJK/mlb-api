2ade318eb23ee2f40a2babb0352b3ffe10c987bb
Angus Kellett <anguskellett@hotmail.com>
2021-11-09 22:57:54 +0000

    adds person symbol to header

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Header.css'
 import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
 
 function Header() {
   return (
     <div className="header">
       <h3>I am the header component</h3>
-      <PersonIcon />
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
     </div>
   )
 }
