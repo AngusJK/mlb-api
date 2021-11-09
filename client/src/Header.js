77b7e1a7232358b6abc521de5cbaeec40b9da086
Angus Kellett <anguskellett@hotmail.com>
2021-11-09 02:28:18 +0000

    initial commit

---
+++ client/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+import PersonIcon from '@mui/icons-material/Person';
+
+function Header() {
+  return (
+    <div className="header">
+      <h3>I am the header component</h3>
+      <PersonIcon />
+    </div>
+  )
+}
+
+export default Header
