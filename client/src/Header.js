326077c70b9ff0df210518fbbc074062c11ac378
Angus Kellett <anguskellett@hotmail.com>
2021-11-10 02:24:06 +0000

    adds MLB logo and forum icon

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,13 +2,17 @@ import React from 'react'
 import './Header.css'
 import PersonIcon from '@mui/icons-material/Person';
 import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
 
 function Header() {
   return (
     <div className="header">
-      <h3>I am the header component</h3>
+      <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2017/04/MLB-Logo.png" alt=""></img>
       <IconButton>
         <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      <IconButton>
+        <ForumIcon fontSize="large" className="forum__icon" /> 
       </IconButton>
     </div>
   )
