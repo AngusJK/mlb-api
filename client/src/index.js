43ad72f9dac813d80a431123b0a66a099192a251
Angus Kellett <anguskellett@hotmail.com>
2021-11-13 23:54:10 +0000

    moves app under playercards

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import Header from './Header.js';
 import App from './App';
 import PlayerCards from './PlayerCards';
@@ -9,13 +8,8 @@ import PlayerCards from './PlayerCards';
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <App />
     <PlayerCards />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
