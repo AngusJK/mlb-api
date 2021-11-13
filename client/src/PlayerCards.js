055f5f75547574c0127297b953ddeefe4abd4eec
Angus Kellett <anguskellett@hotmail.com>
2021-11-13 23:54:59 +0000

    makes axios call (uses breaking bad api as test)

--- client/src/PlayerCards.js
+++ client/src/PlayerCards.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
+import Axios from 'axios'
 import './PlayerCards.css'
 
-function PlayerCards() {
-  // const [player, setPlayer] = useState([
-  //   {
-  //     playerName: 'Vladimir Guerrero',
-  //     imageUrl: ''
-  //   }
-  // ]) 
-  const player = { playerName: 'Vladimir Guerrero', imageUrl: 'https://calltothepen.com/wp-content/uploads/imagn-images/2017/07/16546683.jpeg' }
+const PlayerCards = () => {
+  const [players, setPlayers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    //const apiKey = 'cb2f338924cc4becafb984ba0862d3e4'
+    const fetchItems = async () => {
+      const result = await Axios (`https://www.breakingbadapi.com/api/characters` 
+      // const result = await Axios (`https://api.sportsdata.io/v3/mlb/scores/json/Players/TOR` 
+      // { method: 'GET',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Ocp-Apim-Subscription-Key': `${apiKey}`,
+      //     'Access-Control-Allow-Origin': '*'
+      //   }
+      //}
+      )
+      
+      setPlayers(result.data)
+      setIsLoading(false)
+      console.log(isLoading)
+    }
+    fetchItems()
+  }, [])
   return (
-    <div className="player-cards"> 
-      <h1>{player.playerName}</h1>
-      <div className="player-image">
-        <img  src="https://calltothepen.com/wp-content/uploads/imagn-images/2017/07/16546683.jpeg" alt=""/> 
-      </div> 
+    <div>
+      <h1>Hello from the React MLB API (Player Cards Component)</h1>
+      <h5>{players[0].portrayed}</h5>
+      <h5>{players[1].portrayed}</h5>
+      <h5>{players[2].portrayed}</h5>
+      <h5>{players[3].portrayed}</h5>
+      <h5>{players[4].portrayed}</h5>
     </div>
-  )
+  ) 
 }
 
 export default PlayerCards
+
